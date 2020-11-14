e0af3721f8c7a77634627b8ac31e2afc96fcb1ec
katchen212 <44990409+katchen212@users.noreply.github.com>
2020-11-13 20:59:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function calculateTip () {
+function calculateTip ( ) {
 
 	var billAmount = document.getElementById('billAmount').value;
-	var tip = document.getElementById('tip').value;
+	var Tip = document.getElementById('Tip').value;
 
-	if (billAmount === "" || tip == 0) {
+	if (billAmount === "" || Tip == 0) {
 		window.alert("Please enter some values to run this");
 		return; 
 	}
 
-	var total = (billAmount * tip)
-	total = Math.round(total * 100)/100
+	var total = (billAmount * Tip )
+	total = Math.round(total * 100 )/100
 	total= total.toFixed(2)
 
 	document.getElementById('totalTip').style.display =" block"
 	document.getElementById('tip').innerHTML = total;
+
 
 }
 document.getElementById("totalTip").style.display = "none";
