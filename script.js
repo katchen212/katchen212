51fed947637f638d7f17d851a22051371cb75e38
katchen212 <44990409+katchen212@users.noreply.github.com>
2020-11-13 20:04:18 -0500

    Create script.js
    Javascript for Tip Calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+function calculateTip ( ) {
+
+	var billAmount = document.getElementById('billAmount').value;
+	var Tip = document.getElementById('Tip').value;
+
+	if (billAmount === "" || Tip == 0) {
+		window.alert("Please enter some values to run this");
+		return; 
+	}
+
+	var total = (billAmount * Tip )
+	total = Math.round(total * 100 )/100
+	total= total.toFixed(2)
+
+	document.getElementById('totalTip').style.display =" block"
+	document.getElementById('tip').innerHTML = total;
+
+
+}
+document.getElementById("totalTip").style.display = "none";
+
+ 
+document.getElementById("calculate").onclick = function () { calculateTip();};
